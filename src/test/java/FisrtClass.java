be174e92b860f9c12ddfad63f5f0eac626ad2fa9
semrainanc <55037486+semrainanc@users.noreply.github.com>
2019-11-21 16:37:49 -0500

    removed online

--- src/test/java/FisrtClass.java
+++ src/test/java/FisrtClass.java
@@ -1,6 +1,6 @@
 public class FisrtClass {
     public static void main(String[] args) {
-        System.out.println("Hi World");
+        
         System.out.println("Hello World");
         System.out.println("Merhabe Dunya");
 
