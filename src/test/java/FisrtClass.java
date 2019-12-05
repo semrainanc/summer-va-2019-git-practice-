5e93f184352a066e1aacdf3ad39f4b5dddc051af
semrainanc <55037486+semrainanc@users.noreply.github.com>
2019-12-05 15:27:03 -0500

    Update FisrtClass.java

--- src/test/java/FisrtClass.java
+++ src/test/java/FisrtClass.java
@@ -1,7 +1,7 @@
 public class FisrtClass {
     public static void main(String[] args) {
         
-        System.out.println("Hello Planet");
+        System.out.println("Hello Duru");
         System.out.println("Merhaba Dunya");
         System.out.println("Let`s finish the class");
 
