b83e66bd025cabb9af3a4b40623638276357be1b
semrainanc <55037486+semrainanc@users.noreply.github.com>
2019-12-05 15:45:25 -0500

    Update SecondClass.java

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -3,6 +3,6 @@ public class SecondClass {
         String myname = "Dunya";
         System.out.println(myname);
         System.out.println("This is another line");
-        System.out.println("Duru");
+        System.out.println("Arslan");
     }
 }
